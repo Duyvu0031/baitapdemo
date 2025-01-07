8463a33dd4ef7c5ab6d0717aa7a29b1732d2c365
Duyvu0031 <nguyenvutungduy@gmail.com>
2025-01-07 23:42:34 +0700

    11h42 khuya

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,20 +2,31 @@ class SessionsController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
+   
   def new
+    unless User.exists?(email_address: "duy0031@gmail.com")
+      begin
+        User.create!(email_address: "duy0031@gmail.com", password: "duy0031")
+      rescue ActiveRecord::RecordInvalid => e
+        logger.error "Failed to create admin user: #{e.message}"
+      end
+    end
   end
 
   def create
-    if user = User.authenticate_by(params.permit(:email_address, :password))
+    if user = User.authenticate_by(email_address: params[:email_address], password: params[:password])
       start_new_session_for user
-      redirect_to after_authentication_url
+      session[:user_id] = user.id
+      redirect_to after_authentication_url, notice: "chào mừng bạn: #{user.email_address}!"
     else
-      redirect_to new_session_path, alert: "Try another email address or password."
+      flash.now[:alert] = "Email hoặc mật khẩu không hợp lệ"
+      render :new
     end
   end
 
   def destroy
     terminate_session
-    redirect_to new_session_path
+    session[:user_id] = nil # Xóa thông tin người dùng khỏi session khi đăng xuất
+    redirect_to new_session_path,notice: "Đã đăng xuất thành công"
   end
 end
